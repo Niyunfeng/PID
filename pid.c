08fa17965f160aba2fbb685b4084b7e7c2218c15
Ryan <8788406+Niyunfeng@users.noreply.github.com>
2021-01-29 10:47:44 +0800

    Update format
    Align the comment

--- pid.c
+++ pid.c
@@ -1,26 +1,27 @@
 #define _PID_C
+
 #include "pid.h"
 #include <math.h>
 
-#define MAXOUT 1000				//输出最大值
+#define MAXOUT 1000			//输出最大值
 
 void IncPIDInit(void)
 {
-	sptr			= &sPID;
-	sptr->SetPoint	= 700;		//设定值
-	sptr->BitMove	= 0;		//返回结果比例
+	sptr		 = &sPID;
+	sptr->SetPoint	 = 700;		//设定值
+	sptr->BitMove	 = 0;		//返回结果比例
 
-	sptr->LastError = 0;		//前2次误差值
-	sptr->PrevError = 0;		//前1次误差值
+	sptr->LastError  = 0;		//前2次误差值
+	sptr->PrevError  = 0;		//前1次误差值
 
 	sptr->Proportion = 3;		//比例
 	sptr->Integral	 = 0;		//积分
 	sptr->Derivative = 0;		//微分
 
-	sptr->iError = 0;			//当前误差
-	sptr->iIncpid=0;			//增量误差
+	sptr->iError 	 = 0;		//当前误差
+	sptr->iIncpid	 = 0;		//增量误差
 
-	sptr->Uk = 0;				//输出返回值
+	sptr->Uk         = 0;		//输出返回值
 }
 
 int IncPIDCalc(int NextPoint)
@@ -46,7 +47,7 @@ int IncPIDCalc(int NextPoint)
 	{
 		sptr->Uk = 0;
 	}
-	else	sptr->Uk = sptr->Uk>>sptr->BitMove;
+	else sptr->Uk = sptr->Uk>>sptr->BitMove;
 	
-	return(sptr->Uk);
+	return (sptr->Uk);
 }
